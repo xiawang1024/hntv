a4fae4f520cca5905fe56ffe63d1c44bda5457a4
wangxia/work <xiawang1024@icloud.com>
2018-09-07 10:17:53 +0800

    搜索页完成

--- src/pages/tab3/index.js
+++ src/pages/tab3/index.js
@@ -1,5 +1,6 @@
 import Taro, { Component } from '@tarojs/taro';
 import { View } from '@tarojs/components';
+import { AtIcon } from 'taro-ui';
 import List from '../../components/list/index';
 import Head from '../../components/head/index';
 import './index.scss';
@@ -10,6 +11,9 @@ export default class list extends Component {
 	};
 	constructor(props) {
 		super(props);
+		this.state = {
+			inputValue: ''
+		};
 	}
 	componentWillMount() {}
 
@@ -21,10 +25,47 @@ export default class list extends Component {
 
 	componentDidHide() {}
 	onShareAppMessage = () => {};
+	inputConfirm = () => {
+		Taro.showToast({
+			title: this.state.inputValue,
+			icon: 'success'
+		});
+	};
+	inputHandler = (e) => {
+		let inputValue = e.detail.value;
+		this.setState({
+			inputValue
+		});
+	};
+	clearIptValue = () => {
+		Taro.showToast({
+			title: '清空',
+			icon: 'success'
+		});
+		this.setState({
+			inputValue: ''
+		});
+	};
 	render() {
 		return (
 			<View className='list-wrap'>
 				<Head title='搜索' type='tab' />
+				<View className='sch-wrap'>
+					<AtIcon value='search' size='16' color='#ccc' className='icon-sch' />
+					<Input
+						value={this.state.inputValue}
+						type='text'
+						placeholder='搜索'
+						confirmType='search'
+						focus
+						className='ipt-sch'
+						onConfirm={this.inputConfirm}
+						onInput={this.inputHandler}
+					/>
+					<View className='icon-close-wrap' onClick={this.clearIptValue}>
+						<AtIcon value='close-circle' size='16' color='#ccc' className='icon-close' />
+					</View>
+				</View>
 				<List />
 			</View>
 		);
